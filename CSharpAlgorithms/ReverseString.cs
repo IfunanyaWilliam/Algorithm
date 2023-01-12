160b8318af46b4fd28f10b87343ce112f713457e
IfunanyaWilliam <williamifunanya@gmail.com>
2023-01-12 03:30:17 +0100

    Reverse String

---
+++ CSharpAlgorithms/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgorithms
+{
+    public class ReverseString
+    {
+        public static string Solution(string s)
+        {
+            string newString = "";
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                newString += s[i];
+            }
+            return newString;
+        }
+
+        //public static string Solution1(string str) => string.Join("", str.Reverse());
+    }
+}
---
+++ CSharpAlgorithms/SquareSum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpAlgorithms
+{
+    public class SquareSum
+    {
+        public static int SumOfSquares(int[] n) => n.Sum(i => i * i);
+    }
+}
