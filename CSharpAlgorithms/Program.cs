b6d49c94df4a541bcd152951ed4d57942a9a43da
Ifunanya <williamifunanya@gmail.com>
2023-02-28 09:42:09 +0100

    Returns the smallest positive integer of an array

--- CSharpAlgorithms/Program.cs
+++ CSharpAlgorithms/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpAlgorithms;
+
 Console.WriteLine("Hello, World!");
 
+int[] n = { 2, 4, 3, 9, 8, 18 };
+
+Console.WriteLine(SmallestPositiveInteger.Solution(n));
---
+++ CSharpAlgorithms/SmallestPositiveInteger.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CSharpAlgorithms
+{
+    public static class SmallestPositiveInteger
+    {
+        //Returns the smallest positive integer in an array
+        //int[] A = {1, 2, 3};
+        //Solution(A);      call method with argurment
+
+        public static int Solution(int[] Nums) => Nums.Min();
+        
+
+    }
+}
+
