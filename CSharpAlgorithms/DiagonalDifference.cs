d4d08f30051abd078fa9b5dcaaf0284ec8d1ce82
Ifunanya <williamifunanya@gmail.com>
2025-01-21 16:40:11 +0100

    Add LonelyInteger and DiagonalDifference

---
+++ CSharpAlgorithms/DiagonalDifference.cs
@@ -0,0 +1,30 @@
+﻿
+namespace CSharpAlgorithms
+{
+    public static class DiagonalDifference
+    {
+
+        //Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+        //For example, the square matrix  is shown below:
+
+            //1 2 3
+            //4 5 6
+            //9 8 9  
+
+        public static int CalculateDiagonalDifference(List<List<int>> arr)
+        {
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+            int n = arr.Count; // Assuming arr is a square matrix
+
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagonalSum += arr[i][i]; 
+                secondaryDiagonalSum += arr[i][n - 1 - i];
+            }
+
+            // Return the absolute difference
+            return Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+        }
+    }
+}
---
+++ CSharpAlgorithms/LonelyInteger.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CSharpAlgorithms
+{
+    public static class LonelyInteger
+    {
+        //Given an array of integers, where all elements but one occur twice, find the unique element.
+        //Example
+        //static List<int> a = new List<int> { 1, 2, 3, 2, 1 };
+
+        public static int FindLonelyInteger(List<int> a)
+        {
+            int unique = 0;
+
+            foreach (var number in a)
+            {
+                unique ^= number; // XOR operation
+            }
+
+            return unique; // The result will be the unique number
+        }
+    }
+}
